c402462c5170d0d98efe69281344c1c690fe53a3
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-01 13:33:41 -0400

    Add login route and Update register route

--- api/src/routes/auth.py
+++ api/src/routes/auth.py
@@ -1,3 +1,4 @@
+import datetime
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import create_access_token
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -7,7 +8,34 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['POST'])
 def login():
-    return 'login'
+    # user
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    if not email: return jsonify({'status': 'failed', 'message': 'Email is required', 'data': None}), 400
+    if not password: return jsonify({'status': 'failed', 'message': 'Password is required', 'data': None}), 400
+    
+
+    userExists = User.query.filter_by(email = email).first()
+    # check if user does not exist
+    if not userExists: return jsonify({'status': 'failed', 'message': 'Email/Password are incorrects', 'data': None}), 401
+    # validate password, if the password of the user is different from the password being passed
+    if not check_password_hash(userExists.password, password): 
+        return jsonify({'status': 'failed', 'message': 'Email/Password are incorrects', 'data': None}), 401
+
+    # expire date of access token
+    expires = datetime.timedelta(days=1)
+
+    # create access token    
+    acces_token = create_access_token(identity = userExists.id, expires_delta= expires)
+
+    data = {
+        'access_token': acces_token,
+        'user': userExists.serialize()
+    }
+
+    return jsonify({'status': 'success', 'message:': 'Login successfully', 'data': data}), 200
+
 
 @auth.route('/register', methods=['POST'])
 def register():
@@ -19,14 +47,13 @@ def register():
     name = request.json.get('name', '')
     biography = request.json.get('biography', '')
     
+    if not email: return jsonify({'status': 'failed', 'message': 'Email is required', 'data': None}), 400
+    if not password: return jsonify({'status': 'failed', 'message': 'Password is required', 'data': None}), 400
+
     # check if user already exist
     userFound = User.query.filter_by(email = email).first()
+    if userFound: return jsonify({'status': 'failed', 'message': 'User already exists', 'data': None}), 400
 
-    if userFound: return jsonify({'status': 'failed', 'message': 'User already exist', 'data': None}), 400
-
-    if not email: return jsonify({'status': 'failed', 'message': 'Email is required', 'data': None}), 400
-
-    if not password: return jsonify({'status': 'failed', 'message': 'Password is required', 'data': None}), 400
 
     # if user doesn't exist, create user and create profile
     user = User()
