c2d4450c53529447600b155722984374898a9419
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-01 01:30:20 -0400

    Add User and Profile Models

--- api/src/models.py
+++ api/src/models.py
@@ -1,2 +1,58 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    is_active = db.Column(db.Boolean(), default=True)
+    profile = db.relationship('Profile', backref='user', uselist=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'email': self.email,
+            'is_active': self.is_active
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class Profile(db.Model):
+    __tablename__ = 'profiles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), default='')
+    biography = db.Column(db.String(250), default='')
+    instagram = db.Column(db.String(50), default='')
+    twitter = db.Column(db.String(50), default='')
+    facebook = db.Column(db.String(50), default='')
+    github = db.Column(db.String(50), default='')
+    linkedin = db.Column(db.String(50), default='')
+    users_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'biography': self.biography
+        }
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
