d1475600fa68e3c3c310dc951b1d85a838cb244f
Samuel Palacios <samuelpl0888@gmail.com>
2022-08-01 10:53:50 -0400

    Add register functionality

--- api/src/routes/auth.py
+++ api/src/routes/auth.py
@@ -11,4 +11,36 @@ def login():
 
 @auth.route('/register', methods=['POST'])
 def register():
-    return 'register'
+    # user
+    email = request.json.get('email')
+    password = request.json.get('password')
+
+    # profile
+    name = request.json.get('name', '')
+    biography = request.json.get('biography', '')
+    
+    # check if user already exist
+    userFound = User.query.filter_by(email = email).first()
+
+    if userFound: return jsonify({'status': 'failed', 'message': 'User already exist', 'data': None}), 400
+
+    if not email: return jsonify({'status': 'failed', 'message': 'Email is required', 'data': None}), 400
+
+    if not password: return jsonify({'status': 'failed', 'message': 'Password is required', 'data': None}), 400
+
+    # if user doesn't exist, create user and create profile
+    user = User()
+    user.email = email
+    user.password = generate_password_hash(password)
+
+    profile = Profile()
+    profile.name = name
+    profile.biography = biography
+
+    user.profile = profile
+    user.save()
+
+    # if register succeded
+    if user: return jsonify({'status': 'success', 'message': 'Registered successfully, please login', 'data': None}), 200
+    else: return jsonify({'status': 'failed', 'message': 'Error in register, please try again', 'data': None}), 200
+
